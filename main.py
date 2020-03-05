3b4acb732024dc4eb155c7f5168def188c730b9a
ottohdp <Ottohdp@gmail.com>
2020-03-05 15:09:46 +0100

    v 0.11

--- main.py
+++ main.py
@@ -1,27 +1,59 @@
 import arcade
+import random
 
 # Constants
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 800
-SCREEN_TITLE = "Welcome to Arcade"
-RADIUS = 150
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Cal is on duty"
+RADIUS = 50
+WIDTH = SCREEN_WIDTH - 20
+HEIGHT = SCREEN_HEIGHT - 20
+SCALING = 2
 
 # Open the window
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-# Set the background color
 arcade.set_background_color(arcade.color.WHITE)
 
-# Clear the screen and start drawing
-arcade.start_render()
 
-# Draw a blue circle
-arcade.draw_circle_filled(
-    SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
-)
 
-# Finish drawing
-arcade.finish_render()
 
+
+class Cal_On_Duty(arcade.Window):
+    "Dette er vores take på et Call Of Duty 2d zombies agtigt spil"
+
+    def __init__(self, width , height, title):
+        super().__init__(width, height, title)
+
+        self.enemies.list = arcade.SpriteList()
+        self.all_sprites = arcade.SpriteList()
+
+    def setup(self):
+        self.player = arcade.Sprite("images/Cal_front.png", SCALING)
+        self.player.center_y = self.height/2
+        self.player.center_x = self.width/2
+        self.all_sprites.append(self.player)
+
+    def on_draw(self):
+
+        arcade.start_render()
+        self.all_sprites.draw()
+    # Laver banen
+    arcade.draw_xywh_rectangle_outline(10, 10, WIDTH, HEIGHT, arcade.color.BLACK
+    )
+    arcade.draw_circle_filled(-1, -1, RADIUS, arcade.color.GRAY
+    )
+    arcade.draw_circle_filled(-1, SCREEN_HEIGHT+1, RADIUS, arcade.color.GRAY
+    )
+    arcade.draw_circle_filled(SCREEN_WIDTH+1, SCREEN_HEIGHT+1, RADIUS, arcade.color.GRAY
+    )
+    arcade.draw_circle_filled(SCREEN_WIDTH+1, -1, RADIUS, arcade.color.GRAY
+    )
+
+def main():
+    game = Cal_On_Duty(WIDTH, HEIGHT, SCREEN_TITLE)
+    game.setup()
+    arcade.run()
 # Display everything
-arcade.run()
+
+if __name__ == "__main__":
+    main()
