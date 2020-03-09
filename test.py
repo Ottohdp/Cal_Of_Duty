725f34437b5cef41c4b8d41bec469187127ec564
Eskild <eskildwollesen@live.dk>
2020-03-09 15:07:39 +0100

    Version: 0.1.3

--- test.py
+++ test.py
@@ -11,34 +11,16 @@ SCREEN_TITLE = "Cal is on duty"
 RADIUS = 50
 WIDTH = SCREEN_WIDTH - 20
 HEIGHT = SCREEN_HEIGHT - 20
-SCALING = 2
+SCALING = 0.7
 
 # Classes
 class Welcome(arcade.Window):
     """Main welcome window
     """
     def __init__(self):
-        """Initialize the window
-        """
-
         # Call the parent class constructor
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        # Set the background window
-        arcade.set_background_color(arcade.color.WHITE)
-
-    def setup(self):
-        """Get the game ready to play
-        """
-
-        # Set the background color
-        arcade.set_background_color(arcade.color.WHITE)
-
-        # Set up the player
-        self.player = arcade.Sprite("Images/Cal_front.png", SCALING, image_width=70, image_height=150)
-        self.player.center_y = self.height / 2
-        self.player.center_x = self.width / 2
-        self.all_sprites.append(self.player)
+        self.all_sprites = arcade.SpriteList()
 
     def on_draw(self):
         """Called whenever you need to draw your window
@@ -46,13 +28,24 @@ class Welcome(arcade.Window):
 
         # Clear the screen and start drawing
         arcade.start_render()
-
+        arcade.set_background_color(arcade.color.WHITE)
         arcade.draw_xywh_rectangle_outline(10, 10, WIDTH, HEIGHT, arcade.color.BLACK)
         arcade.draw_circle_filled(-1, -1, RADIUS, arcade.color.GRAY)
         arcade.draw_circle_filled(-1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
         arcade.draw_circle_filled(SCREEN_WIDTH + 1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
         arcade.draw_circle_filled(SCREEN_WIDTH + 1, -1, RADIUS, arcade.color.GRAY)
         self.all_sprites.draw()
+
+    def setup(self):
+        # Set the background color
+
+
+
+        # Set up the player
+        self.player = arcade.Sprite("images/Cal_front.png", SCALING, image_width=70, image_height=150)
+        self.player.center_y = self.height / 2
+        self.player.center_x = self.width / 2
+        self.all_sprites.append(self.player)
 
 class COD(arcade.Window):
     "COD... ish"
@@ -72,4 +65,5 @@ class COD(arcade.Window):
 # Main code entry point
 if __name__ == "__main__":
     app = Welcome()
+    app.setup()
     arcade.run()
