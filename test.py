03680360e0a8fee49ec94024e2325fa67aaeb9dc
Eskild <eskildwollesen@live.dk>
2020-03-12 15:17:23 +0100

    Version: 0.1.3

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 
 # Imports
 import arcade
+import random
 
 # Constants
 SCREEN_WIDTH = 1200
@@ -13,33 +14,32 @@ WIDTH = SCREEN_WIDTH - 20
 HEIGHT = SCREEN_HEIGHT - 20
 SCALING = 0.7
 
+
 # Classes
-class Welcome(arcade.Window):
-    """Main welcome window
-    """
-    def __init__(self):
-        # Call the parent class constructor
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+class FlyingSprite(arcade.Sprite):
+
+    def update(self):
+
+        # Move the sprite
+        super().update()
+
+        # Remove us if we're off screen
+        if self.right < 0:
+            self.remove_from_sprite_lists()
+
+
+class COD(arcade.Window):
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        # Setup the empty sprite lists
+        self.enemies_list = arcade.SpriteList()
         self.all_sprites = arcade.SpriteList()
-
-    def on_draw(self):
-        """Called whenever you need to draw your window
-        """
-
-        # Clear the screen and start drawing
-        arcade.start_render()
-        arcade.set_background_color(arcade.color.WHITE)
-        arcade.draw_xywh_rectangle_outline(10, 10, WIDTH, HEIGHT, arcade.color.BLACK)
-        arcade.draw_circle_filled(-1, -1, RADIUS, arcade.color.GRAY)
-        arcade.draw_circle_filled(-1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
-        arcade.draw_circle_filled(SCREEN_WIDTH + 1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
-        arcade.draw_circle_filled(SCREEN_WIDTH + 1, -1, RADIUS, arcade.color.GRAY)
-        self.all_sprites.draw()
 
     def setup(self):
         # Set the background color
-
-
+        arcade.set_background_color(arcade.color.WHITE)
 
         # Set up the player
         self.player = arcade.Sprite("images/Cal_front.png", SCALING, image_width=70, image_height=150)
@@ -47,23 +47,44 @@ class Welcome(arcade.Window):
         self.player.center_x = self.width / 2
         self.all_sprites.append(self.player)
 
-class COD(arcade.Window):
-    "COD... ish"
+        # Spawn a new enemy every second
+        arcade.schedule(self.add_enemy, 1.0)
 
-    def __init__(self, width, height, title):
-        """Initialize the game
-        """
-        super().__init__(width, height, title)
+        self.paused = False
+        self.collided = False
+        self.collision_timer = 0.0
 
-        # Set up the empty sprite lists
-        self.enemies_list = arcade.SpriteList()
-        self.all_sprites = arcade.SpriteList()
+    def add_enemy(self, delta_time: float):
+        # First, create the new enemy sprite
+        enemy = arcade.Sprite("images/zombie.png", SCALING, image_width=90, image_height=150)
 
+        # Set its position to a random height and off screen right
+        # enemy.left = random.randint(self.width, self.width + 80)
+        # enemy.top = random.randint(10, self.height - 10)
+        enemy.center_y = self.height / 3
+        enemy.center_x = self.width / 3
+
+        # Set its speed to a random speed heading left
+        # enemy.velocity = (random.randint(-20, -5), 0)
+
+        # Add it to the enemies list
+        self.enemies_list.append(enemy)
+        self.all_sprites.append(enemy)
+
+    def on_draw(self):
+        # Clear the screen and start drawing
+        arcade.start_render()
+        # arcade.draw_xywh_rectangle_outline(10, 10, WIDTH, HEIGHT, arcade.color.BLACK)
+        # arcade.draw_circle_filled(-1, -1, RADIUS, arcade.color.GRAY)
+        # arcade.draw_circle_filled(-1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
+        # arcade.draw_circle_filled(SCREEN_WIDTH + 1, SCREEN_HEIGHT + 1, RADIUS, arcade.color.GRAY)
+        # arcade.draw_circle_filled(SCREEN_WIDTH + 1, -1, RADIUS, arcade.color.GRAY)
+        self.all_sprites.draw()
 
 
 
 # Main code entry point
 if __name__ == "__main__":
-    app = Welcome()
+    app = COD(int(SCREEN_WIDTH * SCALING), int(SCREEN_HEIGHT * SCALING), SCREEN_TITLE)
     app.setup()
     arcade.run()
